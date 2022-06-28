d1f2369edbc9a069ebaf987c03653bb1d1dfcb46
shazreenasayd <106991371+shazreenasayd@users.noreply.github.com>
2022-06-29 01:11:14 +0800

    Update login.php

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if (mysqli_num_rows($result) > 0) {
       $_SESSION['email']  = $email;
       $_SESSION['role'] = $role;
       $_SESSION['course'] = $course;
-      header('location: dashboard/index.php');
+      header('location: dashboard');
     }
     else {
       echo "<script>alert('invalid username/password');
