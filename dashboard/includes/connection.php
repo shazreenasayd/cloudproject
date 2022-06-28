4ef168a649f29f57e2a2556e7f3c2b820ef09026
shazreenasayd <106991371+shazreenasayd@users.noreply.github.com>
2022-06-29 01:55:10 +0800

    Update connection.php

--- dashboard/includes/connection.php
+++ dashboard/includes/connection.php
@@ -1,3 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost","root","","notes" ) or die ("error" . mysqli_error($conn));
+
+$db_host = "us-cdbr-east-05.cleardb.net";
+$db_user = "be72a61d3f100c";
+$ds_pass = "8bf7b5a5";
+$db_name = "heroku_cff7bda2a0563de";
+
+$conn = mysqli_connect($db_host,$db_user,$ds_pass,$db_name) or die("database connection error");
 ?>
