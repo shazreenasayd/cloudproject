b4efdd0578be79f3c28996ed721b658dd8156214
shazreenasayd <106991371+shazreenasayd@users.noreply.github.com>
2022-06-28 08:59:58 -0800

    Add files via upload

---
+++ includes/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = "us-cdbr-east-05.cleardb.net";
+$db_user = "be72a61d3f100c";
+$ds_pass = "8bf7b5a5";
+$db_name = "heroku_cff7bda2a0563de";
+
+$conn = mysqli_connect($db_host,$db_user,$ds_pass,$db_name) or die("database connection error");
+?>
