0451c85d5dc8b6adc29438ab8ce0c36333bea8d2
shazreenasayd <106991371+shazreenasayd@users.noreply.github.com>
2022-06-29 02:25:55 +0800

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (isset($_POST['login'])) {
   mysqli_real_escape_string($conn, $username);
   mysqli_real_escape_string($conn, $password);
 $query = "SELECT * FROM users WHERE username = '$username'";
-$result =mysqli_connect($conn , $query) or die("database connection error");
+$result = mysqli_query($conn , $query) or die (mysqli_error($conn));
 if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_array($result)) {
     $id = $row['id'];
@@ -27,7 +27,7 @@ if (mysqli_num_rows($result) > 0) {
       $_SESSION['email']  = $email;
       $_SESSION['role'] = $role;
       $_SESSION['course'] = $course;
-      header('location: dashboard');
+      header('location: dashboard/');
     }
     else {
       echo "<script>alert('invalid username/password');
