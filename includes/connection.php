dbfb4cc89dc026236d9721c37e03afe0833d510f
shazreenasayd <106991371+shazreenasayd@users.noreply.github.com>
2022-06-29 09:28:27 +0800

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -5,5 +5,5 @@ $db_user = "be72a61d3f100c";
 $ds_pass = "8bf7b5a5";
 $db_name = "heroku_cff7bda2a0563de";
 
-$conn = mysqli_query($db_host,$db_user,$ds_pass,$db_name) or die("database connection error");
+$conn = mysqli_connect($db_host,$db_user,$ds_pass,$db_name) or die("database connection error");
 ?>
